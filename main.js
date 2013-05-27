86a6e8b144b702fba70d16a31b4c64a4c03d41da
MrFranke <moskalev.mikhail@gmail.com>
2013-05-27 16:05:56 +0400

    add disable select

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ $.fn.DndTable = function( options ) {
 
     var settings = $.extend({
         CSS_CLASS: 'js-dnd',
-        drop: null,             // Элемент в который можно перетащить строку
+        drop: null,               // Элемент в который можно перетащить строку
         draggableClasses: null,   // Классы, для таблицы, которая создаетс для анимации перетаскивания элемента
-        sort: true              // Вклюение/отключение сортировки в $drop таблице
+        errorClasses: null,       // Эти классы будут навещиваться на перетаскиваемый элемент во время ошибки
+        sort: true,               // Вклюение/отключение сортировки в $drop таблице
+        dropSize: Infinity        // Максимальный размер $drop таблицы
     }, options);
     
     var DndTable = function ( $drag ) {
@@ -15,6 +17,10 @@ $.fn.DndTable = function( options ) {
           , $draggable      // Элемент, который остается в $drop при перетаскивании
           , $clone          // Клон строки с обвязкой в виде таблицы. Этот элемент следует за курсором
           , $cloneTR        // Клон строки, которую нужно переместить. Этот элемент используется для фантома. Он же остается в $drop таблице
+
+          , role                // Назначении таблицы (drag||drop), из которой тянут элемент
+          , numDropElms         // То, сколько элементов в таблице drop
+          , clickable           // Если true, можно перетаскивать элементы, tесли fals
           ;
 
         function init () {
@@ -24,11 +30,18 @@ $.fn.DndTable = function( options ) {
 
         function bindEvents () {
             $drag.on( 'mousedown.DndTable', 'tr',  drag);
-            $drop.on( 'mousedown.DndTable', 'tr',  drag);
+            // Если включена сортировка верхний таблицы
+            if (settings.sort) {$drop.on( 'mousedown.DndTable', 'tr',  drag);}
         }
 
         function updateVars () {
             $drop = settings.drop;
+
+            // Настройки через data-* атребуты
+            settings.dropSize = $drop.data('size')? $drop.data('size') : settings.dropSize;
+            settings.sort = $drop.data('sort')? $drop.data('sort') : settings.sort;
+
+            numDropElms = $drop.find('tr').not('[data-nodrag]').length;
 
             if ( !settings.draggableClasses ) {
                 settings.draggableClasses = $drop.attr('class');
@@ -43,31 +56,33 @@ $.fn.DndTable = function( options ) {
 
             var $choise = $(this)
               , $target = $(e.target)
-              , offsetX = e.offsetX
-              , offsetY = e.offsetY
+              // хаки для FF
+              , offsetX = e.offsetX || e.clientX - $(e.target).offset().left
+              , offsetY = e.offsetY || e.clientY - $(e.target).offset().top;
               ;
+
+            // Не даем выделять текст при перетаскивании
+            document.body.style.webkitUserSelect = 'none';
+            document.body.style.MozUserSelect = 'none';
+
+            role = $choise.parents('table').data('role');
             
             // Делаем перемещаемый элемент прозрачным
-            $draggable = $choise.css({
-                opacity: 0.5
-            });
+            $draggable = $choise.css({opacity: 0.5});
             
-            // Клонируем нужные элементы
+            // Клонируем элементы
             $clone =  makeDraggableEl( $draggable.clone() );
-            $cloneTR = $choise.clone();
-
-            $clone.css({
-                opacity: 0.5,
-                width: $choise.width()
-            });
-
+            $cloneTR = $draggable.clone();
+            //Сохраняем ширину элемента при перетаскивани
+            $clone.css({width: $choise.width()});
             $('body').append( $clone );
 
-            // При сортировке в дропзоне, не нужно показывать фантом
-            if ( $choise.parents('table').data('role') === 'drop' ) {
+
+            // Если элемент перетаскивают из дроп зоны (сортируют)
+            if ( role === 'drop' ) {
+                // При сортировке в дропзоне, не нужно показывать фантом
                 $draggable.hide();
             }
-
             // Проверяем смещение
             if ( $target !== $choise ) {
                 offsetX += $target.offset().left;
@@ -95,13 +110,14 @@ $.fn.DndTable = function( options ) {
             });
 
             // Перемещаем элемент при движении мышки
-            $(document).bind('mousemove.DndTable', function (e) {
+            $(document).bind('mousemove.DndTable', function (e){
                 $('.dragndrop').css({cursor: 'default'});
 
                 $clone.css({
                     left: e.pageX - offsetX,
                     top: e.pageY - offsetY,
                 });
+
 
                 // Если элемент в дропзоне, показываем пользователю его "фантом"
                 if ( checkDropZone(e.pageX, e.pageY) ) {
@@ -119,6 +135,12 @@ $.fn.DndTable = function( options ) {
          * @param pos {Number} индекс элемента, после которого будет следовать перенесенный
          */
         function drop ( pos ) {
+            // Если элементов уже достаточно, не даем положить элемент
+            if ( settings.dropSize === numDropElms && role !== 'drop' ) {
+                returnEl();
+                return false;
+            }
+
             // Перемещаем элемент с курсора в drop таблицу
             var offset = $cloneTR.offset();
             $clone.animate({
@@ -129,25 +151,34 @@ $.fn.DndTable = function( options ) {
                 $cloneTR.css({opacity: 1});
                 $clone.remove()
                 $draggable.remove();
+                
+                // Увеличиваем колличество элеаентов только когда перетаскиваем их в дроп зону
+                if ( role === 'drag' ) {numDropElms++;}
             });
         }
 
         function dropFantom (pos) {
+            $clone.addClass(settings.errorClasses);
             $cloneTR.css({opacity: 0.5});
             $drop.find('tr').eq(pos).after( $cloneTR );
         }
 
         /**
          * Возвращает элемент на его начальную позицию
-         * @param data {Object} позиция элемента
          */
-        function returnEl (data) {
-            $draggable.show(); // Показываем $draggable при сортировке
+        function returnEl () {
+            // Убираем элемент из $drop таблицы
+            if ( role === 'drop' ) {
+                $draggable.prependTo($drag).show();
+                numDropElms--;
+            }
+
             $clone.animate({
                 top: $draggable.offset().top,
                 left: $draggable.offset().left
             }, function () {
                 $clone.remove();
+                $cloneTR.remove();
                 $draggable.css({opacity: 1});
             });
         }
@@ -228,11 +259,34 @@ $.fn.DndTable = function( options ) {
 
             return $el;
         }
+
+        
+        /**
+         * ================== API ===============================
+         */
+
+        /**
+         * Меняет максимальное колличество элементов в drop-таблице
+         * @param m {Number} Значения максимального числа
+         */
+        function setDropSize ( n ) {
+            var diff = n - numDropElms
+              , items = $drop.find('tr').not('[data-nodrag]')
+              , overdrawItems = items.slice(diff)
+              ;
+
+            settings.dropSize = n;
+
+            if ( diff < 0 ) {
+                numDropElms += diff;
+                overdrawItems.each(function(){ $(this).appendTo($drag); }); // Вставляем последнии элементы  в drag таблицу
+            }
+        }
         
         init();
 
         return{
-            
+            setDropSize: setDropSize
         }
     }
 
